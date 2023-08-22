9837dc4fd62b53a3ff90a03a8b02bebaaf76df5c
VVasilevich <vve913@gmail.com>
2023-08-22 20:00:44 +0600

    Add stringMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,35 @@
-import { legacy_createStore as createStore, combineReducers, compose } from 'redux';
+import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
 
-const enhancer = (createStore) => (...args) => {
-  const store = createStore(...args)
-
-  const oldDispatch = store.dispatch
-  store.dispatch = (action) => {
-    if (typeof action === 'string') {
-      return oldDispatch({
-        type: action
-      })
-    }
-    return oldDispatch(action)
+const stringMiddleware = () => (next) => (action) => {
+  if (typeof action === 'string') {
+    return next({
+      type: action
+    })
   }
-  return store
+  return next(action)
 }
+
+// const enhancer = (createStore) => (...args) => {
+//   const store = createStore(...args)
+
+//   const oldDispatch = store.dispatch
+//   store.dispatch = (action) => {
+//     if (typeof action === 'string') {
+//       return oldDispatch({
+//         type: action
+//       })
+//     }
+//     return oldDispatch(action)
+//   }
+//   return store
+// }
 
 const store = createStore(
                 combineReducers({heroes, filters}),
                 compose(
-                  enhancer,
+                  applyMiddleware(stringMiddleware),
                   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
                 ));
 
