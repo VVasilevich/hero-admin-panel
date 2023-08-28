342f1837489b19a76da3293147026a8008385f2d
VVasilevich <vve913@gmail.com>
2023-08-29 00:46:20 +0600

    Add fetchFilters using createAsyncThunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 // import { createAction } from "@reduxjs/toolkit";
 // import { heroesFetching, heroesFetched, heroesFetchingError } from "../components/heroesList/heroesSlice";
-import { filtersFetching, filtersFetched, filtersFetchingError } from "../components/heroesFilters/filtersSlice";
+// import { filtersFetching, filtersFetched, filtersFetchingError } from "../components/heroesFilters/filtersSlice";
 
 // export const fetchHeroes = (request) => (dispatch) => {
 //     dispatch(heroesFetching());
@@ -9,12 +9,12 @@ import { filtersFetching, filtersFetched, filtersFetchingError } from "../compon
 //         .catch(() => dispatch(heroesFetchingError()))
 // }
 
-export const fetchFilters = (request) => (dispatch) => {
-    dispatch(filtersFetching())
-    request("http://localhost:3001/filters")
-        .then(data => dispatch(filtersFetched(data)))
-        .catch(() => dispatch(filtersFetchingError()))
-}
+// export const fetchFilters = (request) => (dispatch) => {
+//     dispatch(filtersFetching())
+//     request("http://localhost:3001/filters")
+//         .then(data => dispatch(filtersFetched(data)))
+//         .catch(() => dispatch(filtersFetchingError()))
+// }
 
 // export const heroesFetching = () => {
 //     return {
--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,19 +1,16 @@
-import { useHttp } from "../../hooks/http.hook";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import classNames from "classnames";
 
-import { fetchFilters } from "../../actions";
-import { activeFilterChanged } from "./filtersSlice";
+import { fetchFilters, activeFilterChanged } from "./filtersSlice";
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
     const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters)
     const dispatch = useDispatch()
-    const {request} = useHttp()
 
     useEffect(() => {
-        dispatch(fetchFilters(request))
+        dispatch(fetchFilters())
     // eslint-disable-next-line
     }, [])
 
